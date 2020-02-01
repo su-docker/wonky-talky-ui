04d3b512c16e5e8d28aed9c89f5ea8ee8d60f275
sreedev <sreedev@icloud.com>
2020-02-01 20:53:49 +0530

    socket connection changes added

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+function subscribe(socket){
+  socket.send(
+    JSON.stringify({
+      "command": "subscribe",
+      "identifier": JSON.stringify({
+        channel: 'WtChannel'
+      })
+    })
+  )
+}
+
 function initialize() {
-  alert("asdfadsf")
+  const socket = new WebSocket('wss://707cccd7.ap.ngrok.io/ptt');
+  socket.onopen = () => {
+    console.log('connected..');
+    subscribe(socket);
+  }
+
+  socket.onmessage = evt => {
+    const evt_data = JSON.parse(evt.data);
+    const message = evt_data.message;
+    if(message){
+      console.log(message.file);
+    }
+  }
+
+  socket.onclose = () => {
+    console.log('disconnected..');
+  }
 }
 
 u(".talk").on('mousedown', function () {
