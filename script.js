297b9d09619dd0b8f0cbd58f22e2f1e417c61535
Sudhakar <sudhakar82@gmail.com>
2020-02-01 20:39:28 +0530

    Add onload listener

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+function initialize() {
+  alert("asdfadsf")
+}
+
 u(".talk").on('mousedown', function () {
-  u(".message").text("Recording...");
+  u(".r-message").text("Recording...");
+  u(".main").addClass("recording");
 })
 
 u(".talk").on('mouseup', function () {
-  u(".message").text("Press to talk");
+  u(".r-message").text("Press to talk");
+  u(".main").removeClass("recording");
 })
+
