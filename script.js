35d72e26a40aef89250869d79b5b40d4070add4b
Rahul <rahul@Railsfactorys-MacBook-Pro.local>
2020-02-01 20:42:03 +0530

    Frontend scripts

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-function subscribe(socket){
+window.onload = function () {
+  attachEventListener();
+  initializeSocket();
+}
+
+var rec;
+
+function startRecording() {
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(handleSuccess);
+}
+
+function handleSuccess(stream) {
+  rec = new MediaRecorder(stream);
+  rec.ondataavailable = sendDataToBackend;
+  rec.start();
+}
+
+function sendDataToBackend(e) {
+  var fd = new FormData()
+  fd.append('audio', e.data)
+  fetch('https://707cccd7.ap.ngrok.io/web_connect', {
+    method: 'POST',
+    body: fd
+  })
+}
+
+
+function attachEventListener() {
+  u(".talk").on('mousedown', function () {
+    u(".r-message").text("Recording...");
+    u(".main").addClass("recording");
+    startRecording();
+  })
+
+  u(".talk").on('mouseup', function () {
+    u(".r-message").text("Press to talk");
+    u(".main").removeClass("recording");
+    if (rec) {
+      rec.stop();
+    }
+  })
+}
+
+function subscribe(socket) {
   socket.send(
     JSON.stringify({
       "command": "subscribe",
@@ -9,17 +54,17 @@ function subscribe(socket){
   )
 }
 
-function initialize() {
+function initializeSocket() {
   const socket = new WebSocket('wss://707cccd7.ap.ngrok.io/ptt');
   socket.onopen = () => {
-    console.log('connected..');
     subscribe(socket);
   }
 
   socket.onmessage = evt => {
     const evt_data = JSON.parse(evt.data);
     const message = evt_data.message;
-    if(message){
+    if (message) {
+      console.log("Got message...");
       console.log(message.file);
     }
   }
@@ -28,14 +73,3 @@ function initialize() {
     console.log('disconnected..');
   }
 }
-
-u(".talk").on('mousedown', function () {
-  u(".r-message").text("Recording...");
-  u(".main").addClass("recording");
-})
-
-u(".talk").on('mouseup', function () {
-  u(".r-message").text("Press to talk");
-  u(".main").removeClass("recording");
-})
-
